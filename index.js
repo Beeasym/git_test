980901110c1ee32ba334c140cccc860f34143952
Vlad <carnifex2022@gmail.com>
2023-08-14 19:08:47 +0400

    new features

--- index.js
+++ index.js
@@ -1,15 +1,1 @@
-const body = document.body;
-
-const heading = document.querySelector('h1');
-
-let originalColor = getComputedStyle(body).backgroundColor;
-
-heading.addEventListener('click', );
-
-function changeBodyColor() {
-    if (body.style.backgroundColor === originalColor) {
-        body.style.backgroundColor = 'green'
-    } else {
-        body.style.backgroundColor = originalColor
-    }
-}
+const lazyLoadElements = document.querySelector(".lazy-load");
