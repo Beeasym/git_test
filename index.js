1904d30ac8927251aedf27ed666b03c9ff4229c3
Vlad <carnifex2022@gmail.com>
2023-08-14 21:25:24 +0400

    add burger menu

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-const lazyLoadElements = document.querySelector(".lazy-load");
+const burgerButton = document.querySelector(".burger");
+
+const burgerItems = document.querySelector(".menu");
+
+burgerButton.addEventListener("click", clickBurger)
+
+function clickBurger() {
+    
+    burgerItems.classList.toggle("open");
+    
+    if (burgerItems.style.display === "block") {
+        burgerItems.style.display = "none";
+    } else {
+        burgerItems.style.display = "block";
+    }
+}
